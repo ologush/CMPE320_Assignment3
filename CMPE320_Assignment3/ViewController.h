1099264092be4ad697f208291fdb5138215bd89d
ologush <17ojl@queensu.ca>
2020-11-02 16:08:56 -0500

    Started the GUI component of assignment 3

--- CMPE320_Assignment3/ViewController.h
+++ CMPE320_Assignment3/ViewController.h
@@ -8,7 +8,39 @@
 
 #import <Cocoa/Cocoa.h>
 
+
+
 @interface ViewController : NSViewController
+
+@property (strong, nonatomic) IBOutlet NSTextField *NumeratorOne;
+
+@property (strong, nonatomic) IBOutlet NSTextField *DenominatorOne;
+
+@property (strong) IBOutlet NSTextField *NumeratorTwo;
+
+@property (strong) IBOutlet NSTextField *DenominatorTwo;
+
+@property (strong) IBOutlet NSTextFieldCell *NumeratorOneCell;
+
+@property (strong) IBOutlet NSNumberFormatter *NumOneValue;
+
+@property (strong) IBOutlet NSNumberFormatter *DenomOneValue;
+
+
+@property (strong) IBOutlet NSNumberFormatter *NumTwoValue;
+
+
+@property (strong) IBOutlet NSNumberFormatter *DenomTwoValue;
+
+@property (strong) IBOutlet NSTextField *Output;
+
+
+
+- (IBAction)SelectOperator:(NSPopUpButton *)sender;
+
+
+- (IBAction)Equals:(NSButton *)sender;
+
 
 
 @end
--- CMPE320_Assignment3/ViewController.m
+++ CMPE320_Assignment3/ViewController.m
@@ -10,6 +10,32 @@
 
 @implementation ViewController
 
+@synthesize NumeratorOne;
+@synthesize DenominatorOne;
+@synthesize NumeratorTwo;
+@synthesize DenominatorTwo;
+
+@synthesize NumOneValue;
+@synthesize DenomOneValue;
+@synthesize NumTwoValue;
+@synthesize DenomTwoValue;
+
+@synthesize NumeratorOneCell;
+
+@synthesize Output;
+
+
+
+int numeratorOne;
+int denominatorOne;
+
+int numeratorTwo;
+int denominatorTwo;
+
+NSString *test = @"";
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
@@ -17,11 +43,37 @@
 }
 
 
+
+
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
 }
+
+
+- (IBAction)SelectOperator:(NSPopUpButton *)sender {
+}
+
+- (IBAction)Equals:(NSButton *)sender {
+    
+    numeratorOne = [NumeratorOne intValue];
+    numeratorTwo = [NumeratorTwo intValue];
+    
+    denominatorOne = [DenominatorOne intValue];
+    denominatorTwo = [DenominatorTwo intValue];
+    
+    int result;
+    
+    Output.stringValue = @"result";
+    
+    
+    
+}
+
+
+
+
 
 
 @end
